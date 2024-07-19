5f097d32273c8341b933f2228a81dc8d44004250
Devansh Garg <devansh261101@gmail.com>
2024-07-19 15:13:32 +0530

    Commiting App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weather from './Components/Weather'
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather />
+    </div>
+  )
+}
+
+export default App
